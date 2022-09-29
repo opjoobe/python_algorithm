1d8d1a65c61b53e3d732c138c1ef0a78eef89c8d
opjoobe <opjoobe@gmail.com>
2022-09-29 16:29:44 +0900

    upload: 220929 algorithm #2

---
+++ Programmers/문자열 내 마음대로 정렬하기.py
@@ -0,0 +1,4 @@
+# 프로그래머스 # 문자열 내 마음대로 정렬하기
+
+def solution(strings, n):
+    return sorted(strings, key = lambda x : (x[n], x))
--- Programmers/부족한 금액 계산하기.py
+++ Programmers/부족한 금액 계산하기.py
@@ -1,5 +1,18 @@
 # 프로그래머스 # 부족한 금액 계산하기
 
+# 첫번째 풀이
 def solution(price, money, count):
     now = price * sum(range(1, count + 1)) - money
     return now if now > 0 else 0
+
+# 두번째 풀이 (한줄풀이)
+
+def solution(price, money, count):
+    return max(price * (1 + count) * count // 2 - money, 0)
+
+# 세번째 풀이 (한줄풀이 v2)
+
+def solution(price, money, count):
+    return max(price * (1 + count) * count // 2 - money, 0)
+
+''' 등차수열을 통해 계산하면 수행속도가 압도적으로 빠르다. '''
