0924fb69ab670c797a97e95f465f7eb5bbd5ce13
opjoobe <opjoobe@gmail.com>
2023-06-13 23:46:55 +0900

    upload: 230613 algorithm

---
+++ Leetcode/509. Fibonacci Number.py
@@ -0,0 +1,8 @@
+# Leetcode # 509. Fibonacci Number
+
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = [0,1] + [0] * (n-1)
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
